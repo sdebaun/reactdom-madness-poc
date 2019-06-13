2080b9cc154e0edc1491e003edcb8b0d2eec6195
sdebaun <sdebaun74@gmail.com>
2019-06-13 12:41:50 -0700

    y u no

---
+++ madness.js
@@ -0,0 +1,16 @@
+// function ReactWrapper(props) {
+//   return props.children
+// }
+
+// window.onload = function() {
+//   let original = window.document.getElementById('original')
+//   let reactEl = document.getElementById('react')
+//   console.log('original', original)
+//   ReactDOM.render(
+//     <ReactWrapper>
+//       original.outerHTML
+//     </ReactWrapper>,
+//     reactEl
+//   )
+// }
+
